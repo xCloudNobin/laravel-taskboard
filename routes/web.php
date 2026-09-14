37f8d3d0f63795bb35713f2f3df366bcc1258b80
neobuilds <neo@nobin.me>
2026-09-14 14:07:47 +0000

    test: induce needs-env
    Co-authored-by: NasirNobin <nasir.nobin@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,3 @@
 <?php
 use Illuminate\Support\Facades\Route;
-Route::get('/', function () { Illuminate\Support\Facades\DB::select('select 1'); return 'qa-v1'; });
+Route::get('/', function () { Illuminate\Support\Facades\DB::select('select 1'); if (!env('QA_SECRET')) { throw new RuntimeException('QA_SECRET missing'); } return 'qa-v1'; });
